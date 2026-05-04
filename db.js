270e7f76dce02ea2f510b86ac0225fd5af6ff70a
LakiyaP <lperry@mail.bradley.edu>
2026-05-04 13:44:19 -0500

    db.js

--- db.js
+++ db.js
@@ -1,6 +1,12 @@
+const mysql = require("mysql2");
+
 const pool = mysql.createPool({
    host: "localhost",
    user: "root",
-   password: "",   // 👈 if you have a password, put it here
-   database: "opstrack_db"
+   password: "",
+   database: "opstrack_db",
+   waitForConnections: true,
+   connectionLimit: 10
 });
+
+module.exports = pool.promise();
